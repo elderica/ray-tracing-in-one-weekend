4c8882ec258402d2dd9fe6a62cac0abae636844c
elderica <1130138+elderica@users.noreply.github.com>
2022-10-10 20:47:17 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -332,3 +332,13 @@ impl HitTable for HitTableList {
         temp_rec
     }
 }
+
+pub type Degrees = f64;
+pub type Radians = f64;
+
+pub const INFINITY: f64 = f64::INFINITY;
+pub const PI: f64 = std::f64::consts::PI;
+
+pub fn degrees_to_radians(degrees: Degrees) -> Radians {
+    degrees * PI / 180.0
+}
