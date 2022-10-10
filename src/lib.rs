66d4551f26cd19785c8b045e1f65e3de57340f36
elderica <1130138+elderica@users.noreply.github.com>
2022-10-10 19:43:03 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::fmt::Display;
-use std::ops;
+use std::ops::{self, Neg};
 
 pub struct Color {
     r: f64,
@@ -205,5 +205,94 @@ impl Ray {
 
     pub fn at(&self, t: f64) -> Point3 {
         self.origin + t * self.direction
+    }
+}
+
+pub struct HitRecord {
+    p: Point3,
+    normal: Vec3,
+    t: f64,
+    front_face: bool,
+}
+
+impl HitRecord {
+    pub fn new(p: Point3, t: f64) -> Self {
+        let front_face = false;
+        let normal = Vec3::zero();
+        Self {
+            p,
+            normal,
+            t,
+            front_face,
+        }
+    }
+
+    pub fn set_face_normal(&self, r: &Ray, outward_normal: &Vec3) -> Self {
+        let p = self.p;
+        let t = self.t;
+        let front_face = dot(r.direction(), outward_normal) < 0.0;
+        let normal = if front_face {
+            *outward_normal
+        } else {
+            outward_normal.neg()
+        };
+        Self {
+            p,
+            normal,
+            t,
+            front_face,
+        }
+    }
+}
+
+pub trait HitTable {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
+}
+
+pub struct Sphere {
+    center: Point3,
+    radius: f64,
+}
+
+impl Sphere {
+    pub fn new(center: Point3, radius: f64) -> Self {
+        Self { center, radius }
+    }
+
+    pub fn center(&self) -> &Point3 {
+        &self.center
+    }
+    pub fn radius(&self) -> f64 {
+        self.radius
+    }
+}
+
+impl HitTable for Sphere {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let oc = *r.origin() - self.center;
+        let a = r.direction().length_squared();
+        let half_b = dot(&oc, r.direction());
+        let c = oc.length_squared() - self.radius() * self.radius();
+
+        let discriminant = half_b * half_b - a * c;
+        if discriminant < 0.0 {
+            return None;
+        }
+        let sqrtd = discriminant.sqrt();
+
+        let root = (-half_b - sqrtd) / a;
+        if root < t_min || t_max < root {
+            let root = (-half_b + sqrtd) / a;
+            if root < t_min || t_max < root {
+                return None;
+            }
+        }
+
+        let t = root;
+        let p = r.at(t);
+        let outward_normal = (p - self.center) / self.radius;
+        let h = HitRecord::new(p, t).set_face_normal(r, &outward_normal);
+
+        Some(h)
     }
 }
