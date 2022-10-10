e2833d6a25056a3c6f53fbb66c3621f3b44e2b8d
elderica <1130138+elderica@users.noreply.github.com>
2022-10-10 18:04:59 +0900

    Image 4

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 use ray_tracing_in_one_weekend::{dot, unit_vector, Color, Point3, Ray, Vec3};
 use std::io::{self, BufWriter, Write};
 
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
     let oc = *r.origin() - *center;
     let a = dot(r.direction(), r.direction());
     let b = 2.0 * dot(&oc, r.direction());
     let c = dot(&oc, &oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
 
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
+}
+
+fn vec3_to_color(v: Vec3) -> Color {
+    Color::new(v.x(), v.y(), v.z())
 }
 
 fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        // return Color::new(1.0, 0.0, 0.0);
+        let a = r.at(t) - Vec3::new(0.0, 0.0, -1.0);
+        let n = unit_vector(&a);
+        return vec3_to_color(0.5 * Vec3::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0));
     }
     let unit_direction = unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
