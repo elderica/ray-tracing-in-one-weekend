74aa4ea8751c60f9f9b4594f764ff714c7b703b6
elderica <1130138+elderica@users.noreply.github.com>
2022-10-27 21:59:15 +0900

    Simplify main code

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,42 @@
 use rand::{self, Rng};
 use std::{
-    cell::{Ref, RefCell},
     io::{self, BufWriter, Write},
     rc::Rc,
 };
 
-mod camera;
-mod color;
-mod hittable;
 mod material;
-mod ray;
-mod rtweekend;
-mod vec3;
+use material::{Lambertian, Material, Metal};
 
-fn ray_color(r: &ray::Ray, world: Ref<dyn hittable::HitTable>, depth: i32) -> vec3::Color {
+mod vec3;
+use vec3::{Color, Point3};
+
+mod hittable;
+use hittable::{HitTable, HitTableList, Sphere};
+
+mod camera;
+use camera::Camera;
+
+mod ray;
+use ray::Ray;
+
+mod color;
+mod rtweekend;
+
+fn ray_color(r: &Ray, world: &impl HitTable, depth: i32) -> Color {
     if depth <= 0 {
-        return vec3::Color::zero();
+        return Color::zero();
     }
 
     if let Some(rec) = world.hit(r, 0.001, rtweekend::INFINITY) {
         if let Some((attenuation, scattered)) = rec.material.scatter(r, &rec) {
             return attenuation * ray_color(&scattered, world, depth - 1);
         }
-        return vec3::Color::zero();
+        return Color::zero();
     }
 
     let unit_direction = vec3::unit_vector(&r.direction);
     let t = 0.5 * (unit_direction.y() + 1.0);
-    (1.0 - t) * vec3::Color::new(1.0, 1.0, 1.0) + t * vec3::Color::new(0.5, 0.7, 1.0)
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
 fn main() -> io::Result<()> {
@@ -37,33 +46,29 @@ fn main() -> io::Result<()> {
     let samples_per_pixel = 100;
     let max_depth = 50;
 
-    let world = Rc::new(RefCell::new(hittable::HitTableList::default()));
-    let material_ground: Rc<dyn material::Material> =
-        Rc::new(material::Lambertian::new(vec3::Color::new(0.8, 0.8, 0.0)));
-    let material_center: Rc<dyn material::Material> =
-        Rc::new(material::Lambertian::new(vec3::Color::new(0.7, 0.3, 0.3)));
-    let material_left: Rc<dyn material::Material> =
-        Rc::new(material::Metal::new(vec3::Color::new(0.8, 0.8, 0.8)));
-    let material_right: Rc<dyn material::Material> =
-        Rc::new(material::Metal::new(vec3::Color::new(0.8, 0.6, 0.2)));
+    let mut world = HitTableList::default();
+    let material_ground: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
+    let material_center: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
+    let material_left: Rc<dyn Material> = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8)));
+    let material_right: Rc<dyn Material> = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2)));
 
-    world.borrow_mut().add(Rc::new(hittable::Sphere::new(
-        vec3::Point3::new(0.0, -100.5, -1.0),
+    world.add(Rc::new(Sphere::new(
+        Point3::new(0.0, -100.5, -1.0),
         100.0,
         Rc::clone(&material_ground),
     )));
-    world.borrow_mut().add(Rc::new(hittable::Sphere::new(
-        vec3::Point3::new(0.0, 0.0, -1.0),
+    world.add(Rc::new(Sphere::new(
+        Point3::new(0.0, 0.0, -1.0),
         0.5,
         Rc::clone(&material_center),
     )));
-    world.borrow_mut().add(Rc::new(hittable::Sphere::new(
-        vec3::Point3::new(-1.0, 0.0, -1.0),
+    world.add(Rc::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
         0.5,
         Rc::clone(&material_left),
     )));
-    world.borrow_mut().add(Rc::new(hittable::Sphere::new(
-        vec3::Point3::new(1.0, 0.0, -1.0),
+    world.add(Rc::new(Sphere::new(
+        Point3::new(1.0, 0.0, -1.0),
         0.5,
         Rc::clone(&material_right),
     )));
@@ -79,7 +84,7 @@ fn main() -> io::Result<()> {
         image_width, image_height
     )?;
 
-    let cam = camera::Camera::new();
+    let cam = Camera::new();
     let mut rng = rand::thread_rng();
     for j in (0..image_height).rev() {
         write!(err, "\rScan lines remaining: {} ", j)?;
@@ -90,7 +95,7 @@ fn main() -> io::Result<()> {
                 let u = (f64::from(i) + rng.gen::<f64>()) / f64::from(image_width - 1);
                 let v = (f64::from(j) + rng.gen::<f64>()) / f64::from(image_height - 1);
                 let r = cam.get_ray(u, v);
-                pixel_color += ray_color(&r, world.borrow(), max_depth);
+                pixel_color += ray_color(&r, &world, max_depth);
             }
             color::write_color(&mut bufout, pixel_color, samples_per_pixel)?;
         }
